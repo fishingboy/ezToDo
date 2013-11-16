729a7fd077d140b94b0a719bf86a65d3b19bbbbb
wbkuo <et282523@hotmail.com>
2013-11-16 16:06:32 +0800

    加上 facnybox (直接用 camdemy 的)

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -7,7 +7,9 @@
 <!-- fancyBox -->
 <!-- Add jQuery library -->
 <script type="text/javascript" src="sys/js/jquery.js"></script>
+<script type="text/javascript" src="sys/js/jquery.fancybox.js"></script>
 <style type="text/css">@import URL("sys/css/main.css");</style>
+<style type="text/css">@import URL("sys/css/jquery.fancybox.css");</style>
 </head>
 <body>
 <div id='base'>
--- application/views/show_view.php
+++ application/views/show_view.php
@@ -1,17 +1,29 @@
 <script type="text/javascript">
-$(function() 
+// 編輯
+function to_edit() 
 {
-    $('#fmAdd').bind('click', function() 
+    $.fancybox(
     {
-        console.log('add');
-        $(".fancybox").fancybox();
+        'type'      : 'iframe',
+        'href'      : 'http://wbkuo.twbbs.org',
+        'margin'    : 60,
+        'padding'   : 5,
+        'width'     : 500,
+        'height'    : 500,
+        'autoSize'  : false
     });
+}
+
+$(function() 
+{   
+    // 綁定事件
+    $('#fmAdd').bind('click', to_edit);
 });
 </script>
 
 <input id='fmAdd' type='button' value='新增'>
-<a class="fancybox" rel="group" href="big_image_1.jpg"><img src="small_image_1.jpg" alt="" /></a>
-<a class="fancybox" rel="group" href="big_image_2.jpg"><img src="small_image_2.jpg" alt="" /></a>
+<a id='f1' class="fancybox" rel="group" href="big_image_1.jpg"><img src="small_image_1.jpg" alt="" /></a>
+<a id='f2' class="fancybox" rel="group" href="big_image_2.jpg"><img src="small_image_2.jpg" alt="" /></a>
 <table class='list'>
     <tr class='header'>
         <th>工作名稱</th>
