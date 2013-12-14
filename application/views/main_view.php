f60775f0681eb44e0f8301c58676e36bb547ce04
wbkuo <et282523@hotmail.com>
2013-12-14 21:37:29 +0800

    修正檔案路徑

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -6,10 +6,10 @@
 <title>Easy ToDo</title>
 <!-- fancyBox -->
 <!-- Add jQuery library -->
-<script type="text/javascript" src="/sys/js/jquery.js"></script>
-<script type="text/javascript" src="/sys/js/jquery.fancybox.js"></script>
-<style type="text/css">@import URL("/sys/css/main.css");</style>
-<style type="text/css">@import URL("/sys/css/jquery.fancybox.css");</style>
+<script type="text/javascript" src="sys/js/jquery.js"></script>
+<script type="text/javascript" src="sys/js/jquery.fancybox.js"></script>
+<style type="text/css">@import URL("sys/css/main.css");</style>
+<style type="text/css">@import URL("sys/css/jquery.fancybox.css");</style>
 </head>
 <body>
 <div id='base'>
