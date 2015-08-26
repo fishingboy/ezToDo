f3a7e07f4efc377929ec70dda77819defa27e4df
Leo.Kuo <et282523@hotmail.com>
2015-08-26 12:09:35 +0800

    先隨便刻一個登出

---
+++ application/views/page/navi_view.php
@@ -0,0 +1,9 @@
+<style>
+.navi       {margin-bottom: 20px; background: #EEE;}
+.navi .item {}
+</style>
+
+<div class='navi'>
+    <!-- <div class='item'>登入</div> -->
+    <div class='item'><a href='/page/login/logout'>登出</a></div>
+</div>
