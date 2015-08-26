85f067396bbc66805628ce99a7c4694bb7aad6df
Leo.Kuo <et282523@hotmail.com>
2015-08-26 17:55:26 +0800

    計算完工時間的 Library - Worktime -- Worktime::get_due_time() - 取得完工時間 Worktime::get_week() - 取得當天星期幾 Worktime::get_config() - 取得工作時間的設定

---
+++ application/config/worktime.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 定義工作時間
+ * 如: 星期一 ~ 星期五  09:30  ~ 18:30
+ */
+$config['worktime'] = [
+    1 => ['start' => '09:30', 'end' => '18:30'],
+    2 => ['start' => '09:30', 'end' => '18:30'],
+    3 => ['start' => '09:30', 'end' => '18:30'],
+    4 => ['start' => '09:30', 'end' => '18:30'],
+    5 => ['start' => '09:30', 'end' => '18:30'],
+];
+
+
+/* End of file worktime.php */
+/* Location: ./application/config/worktime.php */
--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Test extends CI_Controller 
+class Test extends CI_Controller
 {
 	public function __construct()
 	{
@@ -11,7 +11,7 @@ class Test extends CI_Controller
 	{
         $array = array
         (
-            'userID'   => 1, 
+            'userID'   => 1,
             'userName' => 'Leo.kuo'
         );
         $this->session->set_userdata($array);
@@ -21,5 +21,17 @@ class Test extends CI_Controller
     {
         echo $this->session->userdata('userID');
         echo $this->session->userdata('userName');
+    }
+
+    public function work()
+    {
+        header("Content-Type:text/html; charset=utf-8");
+        $this->load->library('worktime');
+        $time = "2015-08-23";
+        echo "<pre>week = " . print_r($this->worktime->get_week($time), TRUE). "</pre>";
+
+        $now = date("Y-m-d H:i:s");
+        echo "now = $now <br>";
+        echo "<pre>get_due_time = " . print_r($this->worktime->get_due_time($now, 2), TRUE). "</pre>";
     }
 }
